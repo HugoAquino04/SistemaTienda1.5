cfa01dd565b1f06622c6c8f6266762a33784754e
hugoa <hugoa@HUGO-AQUINO>
2024-04-05 09:03:12 -0600

    TablaGDTambienMigracion

---
+++ SistemaTienda1.5/Models/GrupoDescuento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SistemaTienda1._5.Models
+{
+    [Table("GrupoDescuento")]
+    public class GrupoDescuento
+    {
+        [Key]
+        public int GrupoDescuentoId { get; set; }
+
+        [MaxLength(50)]
+        public string Codigo { get; set; }
+
+        [MaxLength(50)]
+        public string Descripcion { get; set; }
+
+        public bool Estado { get; set; }
+
+        public int Porcentaje { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+    }
+}
