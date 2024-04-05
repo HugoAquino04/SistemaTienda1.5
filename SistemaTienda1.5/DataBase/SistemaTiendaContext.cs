075e83a1f20f9c31325640754c6c39fc349b7e37
hugoa <hugoa@HUGO-AQUINO>
2024-04-05 08:49:38 -0600

    Agregar archivos de proyecto.

---
+++ SistemaTienda1.5/DataBase/SistemaTiendaContext.cs
@@ -0,0 +1,27 @@
+﻿using SistemaTienda1._5.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace SistemaTienda1._5.DataBase
+{
+    public class SistemaTiendaContext:DbContext
+    {
+        public SistemaTiendaContext():base("name = SistemaTienda")
+        {
+            
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
---
+++ SistemaTienda1.5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SistemaTienda1._5.MvcApplication" Language="C#" %>
